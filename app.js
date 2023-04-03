0002a5db554cbe79b8175632fbba3af245c36996
felamps <felipegabriel1106@outlook.com]>
2023-04-03 12:13:54 -0300

    atualizando app js e adicionando duas linhas

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("Entendendo como funciona o GitHub, estudando na Fomenta pela ALURA")
+console.log("Adicionando mais uma linha para atualizar o commit através do cmd")
+console.log("Mais uma vez, tentando dnv")
