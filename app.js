51b8d5628cb21a465bf71457aaa9319610647275
felamps <felipegabriel1106@outlook.com]>
2023-04-04 09:00:16 -0300

    voltando no tempo para o commit antigo

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Entendendo como funciona o GitHub, estudando na Fomenta pela ALURA")
-console.log("Atualizando e entendendo o comando git restore")
-console.log("Visualizando o código fonte e ele não corrige automático")
+console.log("Adicionando mais uma linha para atualizar o commit através do cmd")
+console.log("Mais uma vez, tentando dnv")
